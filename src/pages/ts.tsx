7272325e9002e58a6aba9516441b1c7ce44b87c9
hansnow <hansnow2012@gmail.com>
2018-05-10 15:43:39 +0800

    Deps: add react-document-title

--- src/pages/ts.tsx
+++ src/pages/ts.tsx
@@ -1,6 +1,11 @@
 import React, { SFC } from 'react'
 import { Button } from 'antd-mobile'
+import DocumentTitle from 'react-document-title'
 
-const TsPage: SFC = () => <Button>This is Ts Page</Button>
+const TsPage: SFC = () => (
+  <DocumentTitle title="TypeScript Powered">
+    <Button>This is Ts Page</Button>
+  </DocumentTitle>
+)
 
 export default TsPage
