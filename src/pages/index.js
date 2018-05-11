39e31eabdc15b59ee5d6d5f3d08107dfcf53f6cf
hansnow <hansnow2012@gmail.com>
2018-05-11 14:39:22 +0800

    Deps: add styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import Link from 'umi/link'
+import DocumentTitle from 'react-document-title'
 import { Button } from 'antd-mobile'
+import styled from 'styled-components'
+
+const StyledButton = styled(Button)`
+  &.am-button-primary {
+    background-color: #000;
+    border: 1px solid #000;
+  }
+`
 
 export default () => (
-  <div>
-    <Link to="/counter">
-      <Button>Counter</Button>
-    </Link>
-  </div>
+  <DocumentTitle title="Index">
+    <>
+      <Link to="/counter">
+        <Button>Counter</Button>
+      </Link>
+      <StyledButton type="primary">Styled Button</StyledButton>
+    </>
+  </DocumentTitle>
 )
